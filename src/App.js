d73baee8484d2885a9e6db7d67058d5d8c0ea551
JairoCardenas123 <CardenasJairo102011@gmail.com>
2023-07-26 13:18:31 -0500

    REACTTTTTTTTTTTTTTT

--- src/App.js
+++ src/App.js
@@ -3,23 +3,12 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <h1>Bienvenido a CampusLands</h1>
+        <p>Explora nuestros campus y descubre nuevas experiencias</p>
+      </div>
   );
 }
+
 
 export default App;
